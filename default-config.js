65d7f05d923ea1274fcdd0835201847c553f32d3
Rasmus Eskola <fruitiex@gmail.com>
2015-03-18 23:53:00 +0200

    these variables were missing from the config

--- default-config.js
+++ default-config.js
@@ -2,6 +2,8 @@ var nodeplayerConfig = require('nodeplayer-config');
 
 var defaultConfig = {};
 
+defaultConfig.badVotePercent = 0.51; // remove songs where (downVotes / totalVotes) > this
+defaultConfig.songMaxDuration = 8 * 60 * 1000; // max allowed song duration
 defaultConfig.setAsRoot = true; // should requests to '/' be redirected to this module?
 
 module.exports = defaultConfig;
